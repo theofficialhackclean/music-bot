4437b2db102b5e2ddc7ee1195b2229b27516fabb
theofficialhackclean <85363951+theofficialhackclean@users.noreply.github.com>
2021-06-04 22:12:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODUwNDA5MzQ4NDI4OTg4NDI2.YLpTbA.UhxLQxmRjhbN0HNFw-8RmpKXnzs',
+        prefix: '.',
+        activity: '.help for any help with the bot ',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
